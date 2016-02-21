92809f3dabdeaf5d4fb88d742b0233693d156fb2
elpatron68 <elpatron@cepheus.uberspace.de>
2016-02-21 13:30:37 +0100

    Don´t remember

--- matterslash.py
+++ matterslash.py
@@ -4,6 +4,7 @@
 """
 This is a very basic server application that receives Mattermost slash commands.
 Set up your slash commands as described in http://docs.mattermost.com/developer/slash-commands.html
+
 As a sample, a /weather report using OpenWeatherMaps is included.
 Feel free to fork and expand this project.
 
@@ -43,7 +44,6 @@ class MattermostRequest:
 class PostHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         """Respond to a POST request."""
-
         # Extract the contents of the POST
         length = int(self.headers['Content-Length'])
         post_data = urlparse.parse_qs(self.rfile.read(length).decode('utf-8'))
@@ -80,14 +80,13 @@ class PostHandler(BaseHTTPRequestHandler):
 
         if responsetext:
             data = {}
+            # 'response_type' may also be 'in_channel'
             data['response_type'] = 'ephemeral'
             data['text'] = responsetext
-
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data))
-
             # {
             # "response_type": "in_channel",
             # "text": "This is some response text."
@@ -102,7 +101,7 @@ def getweather(city):
         searchstring = 'Kiel,de'
 
     # Get your own API key from http://openweathermap.org/api
-    # You can change the language setting here:
+    # You can also change the language setting here:
     owm = pyowm.OWM(API_key='skjdjf98423322fjwesjfwefj898jklf', language='en')
     observation = owm.weather_at_place(searchstring)
     w = observation.get_weather()
@@ -123,20 +122,17 @@ def getweather(city):
 def dosomething(text, username):
     username = ''.join(username).encode('latin1')
     text = ''.join(text).encode('latin1')
-
     return u'#### Hello ' + username + '!\n This is what you wrote: `' + text + "`."
 
 
 def dosomethingelse(text, username):
     username = ''.join(username).encode('latin1')
     text = ''.join(text).encode('latin1')
-
     return u'#### Moin ' + username + ' as we say in northern Germany. What did you mean with `' + text + "`?"
 
 
 if __name__ == '__main__':
     from BaseHTTPServer import HTTPServer
-
     server = HTTPServer((HOSTNAME, PORT), PostHandler)
     print('Starting matterslash server, use <Ctrl-C> to stop')
     server.serve_forever()
